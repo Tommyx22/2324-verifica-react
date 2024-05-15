eb93e8d773efcd978252d791f7b527bfebc48321
lai2-24 <lai2-24@itismeucci.com>
2024-05-15 13:16:06 +0200

    tutto ok

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+
+  const [numero, setNumero] = useState(null);
+  const [id, setId] = useState(null);
+  const [guess, setGuess] = useState(null);
+  const [tentativi, setTentativi] = useState(0);
+  const [result, setResult] = useState('');
+  
+
+  async function AvvioPartita() {
+    const response = await fetch("http://localhost:8080/partita", {method: "POST"});
+    const data = await response.json();
+    setId(data.id);
+    setNumero(null);
+    setTentativi(0);
+    setGuess(data.numero);
+    setResult('');
+  }
+
+  async function InvioTentativo() {
+    const response = await fetch(`http://localhost:8080/partita/${id}`, {method: "PUT" , 
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({numero: parseInt(numero)})
+    }
+    );
+    const data = await response.json();
+    setTentativi(data.tentativi);
+    if(data.risultato === 0) {
+      setResult("Congratulazioni hai indovinato il numero");
+    } else if(data.risultato === -1) {
+      setResult("il numero è troppo piccolo");
+    } else {
+      setResult("il numero è troppo grande");
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h2>Indovina Numero</h2>
+      <button onClick={AvvioPartita}>Nuova partita</button>
+
+          { id &&
+
+            <>
+              <p>ID: {id}</p>
+              <p>Tentativi: {tentativi}</p>
+              <p>Numero: {guess}</p>
+              <div>
+                <input type="number" value={numero} onChange={(e) =>
+                setNumero(e.target.value)} placeholder='numero...'/>
+                <button onClick={InvioTentativo}>Invia</button>
+              </div>
+              <p>{result}</p>
+            </>
+          }
     </div>
-  );
+);
 }
 
 export default App;
